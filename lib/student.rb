1689503d050eb09537fc4cf3c31fc9ef67c31596
Monica Marziano <mmarziano1@gmail.com>
2019-01-23 01:39:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -97,7 +97,13 @@ class Student
   end 
   
   def self.first_X_students_in_grade_10
+    sql = <<-SQL
+      SELECT * 
+      FROM students
+      WHERE grade = 10
+    SQL
     
+    DB[:conn].execute(sql)
   end 
   
   def self.first_X_students_in_grade_10
